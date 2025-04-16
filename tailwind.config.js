8c94556d77746f6166eb5e33d96e56442658167f
Dora <dorastegkou@gmail.com>
2025-04-16 22:33:11 +0300

    Added safelist to config file for dynamically inserted colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-red-500",
+    "hover:bg-red-500",
+    "bg-lime-500",
+    "hover:bg-lime-500",
+  ],
   theme: {
     extend: {},
   },
